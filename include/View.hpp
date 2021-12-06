af4374711c6352d26c4d6bb215277083cabf3d6a
arthur.nogueira <arthur.nogueira@codex.com.br>
2021-12-06 09:19:14 -0300

    Creating Controller and View

---
+++ include/View.hpp
@@ -0,0 +1,26 @@
+#include "../include/Car.hpp"
+#include <fstream>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_keyboard.h>
+#include <vector>
+#include <memory>
+#include <math.h>
+
+class View
+{
+private:
+    /* data */
+public:
+    View(/* args */);
+    ~View();
+};
+
+View::View(/* args */)
+{
+}
+
+View::~View()
+{
+}
